2aa168921b68e18492078be0b6f2d9083d6202a7
tristen <tristen.brown@gmail.com>
2019-05-05 16:10:14 -0400

    Updates

---
+++ precache-manifest.fd8405baf66809d4f92a0c1b9df9ee29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bf602b943f7cf2489bfee89399da87",
+    "url": "/tristen/inverted-polygons/index.html"
+  },
+  {
+    "revision": "7605af0702efc537d26e",
+    "url": "/tristen/inverted-polygons/static/js/2.36fa26f9.chunk.js"
+  },
+  {
+    "revision": "dfe82b57364dc81a8321",
+    "url": "/tristen/inverted-polygons/static/js/main.12091962.chunk.js"
+  },
+  {
+    "revision": "663e2f8f0ea6df49584f",
+    "url": "/tristen/inverted-polygons/static/js/runtime~main.272274ee.js"
+  }
+]);
