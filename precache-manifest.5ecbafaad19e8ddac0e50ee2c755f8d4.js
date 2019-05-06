547aed4e44e50d00f251c9f644bacb3a9cbef02f
tristen <tristen.brown@gmail.com>
2019-05-05 20:10:52 -0400

    Updates

---
+++ precache-manifest.5ecbafaad19e8ddac0e50ee2c755f8d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b56fc51eba1b4bf8314043095990c40",
+    "url": "/inverted-polygons/index.html"
+  },
+  {
+    "revision": "7605af0702efc537d26e",
+    "url": "/inverted-polygons/static/js/2.36fa26f9.chunk.js"
+  },
+  {
+    "revision": "dfe82b57364dc81a8321",
+    "url": "/inverted-polygons/static/js/main.12091962.chunk.js"
+  },
+  {
+    "revision": "9c77b70c7a299db00168",
+    "url": "/inverted-polygons/static/js/runtime~main.02d1899b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77a6c8d4d928b9e8e253c262f5559cd8.js"
+  "/inverted-polygons/precache-manifest.5ecbafaad19e8ddac0e50ee2c755f8d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inverted-polygons/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
