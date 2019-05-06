5c30da22b32000460afe8c768cb4b1f5bcc759b1
tristen <tristen.brown@gmail.com>
2019-05-05 20:33:38 -0400

    Fix cli

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Inverted polygons
 ---
 
-Create inverted polygons with ease!
+Create inverted polygons with ease. Use the [standalone demo](http://tristen.ca/inverted-polygons/) or commandline app.
+
+#### Commandline instructions
+
+```bash
+$ npm install -g inverted-polygons
+
+# Send input via stdin
+$ cat polygon.geojson | inverted-polygons > polygon-inverted.geojson
+```
--- bin/inverted-polygons.js
+++ bin/inverted-polygons.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
-const meow = require('meow');
 const invertPolygons = require('../src/util/invert-polygons');
 
-const cli = meow(
-  `
+const HELP =
+`
   Provide GeoJSON from stdin and get back GeoJSON with polygons inverted.
   Usage
     $ cat polygons.geojson | invertedPolygons > polygons-inverted.geojson
-`
-);
+`;
 
 let rawInput = '';
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-  if (!chunk) exit();
-  rawInput += chunk;
+  if (chunk !== null) {
+    rawInput += chunk;
+  }
 });
 
 process.stdin.on('end', () => {
-  const geojson = invertPolygons(rawInput);
+  const geojson = invertPolygons(JSON.parse(rawInput));
   if (!geojson) exit();
-  process.stdout.write(`${JSON.stringify(geojson, null, 2)}\n`);
+  process.stdout.write(`${JSON.stringify(geojson)}\n`);
 });
 
 function exit() {
-  process.stdout.write(cli.showHelp());
+  process.stdout.write(HELP);
   process.exit();
 }
